ce4732baf6549a01bfb1dcb77a2db740ef065aef
tamij12 <tamijangosrz@gmail.com>
2025-01-08 14:45:56 -0600

    configuration firts step - daisyui

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+  // theme: {
+  //   extend: {
+  //     colors: {
+  //       background: "var(--background)",
+  //       foreground: "var(--foreground)",
+  //     },
+  //   },
+  // },
+  daisyui: {
+    themes: ["corporate"],
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
 };
